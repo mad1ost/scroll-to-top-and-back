30c79dbc28ded97efac4f99063434d16446f8d07
mad1ost <mad1ost@mail.ru>
2020-06-28 21:14:05 +0300

    refactoring

--- background.js
+++ background.js
@@ -1,32 +1,26 @@
 'use strict';
 
-const savedPageYOffsets = {}; // with tabId as keys
+const code = `
+  !(function() {
+    let prevY = window.prevY;
+    let top;
+    if (window.scrollY === 0) {
+      if (!prevY) return;
+      top = prevY;
+    } else {
+      prevY = window.scrollY;
+      top = 0;
+    }
+    window.scrollTo({
+      top: top
+    });
+    window.prevY = prevY;
+  }());
+`;
 
 browser.browserAction.onClicked.addListener((tab) => {
   browser.tabs.executeScript({
-    code: 'window.pageYOffset',
+    code: code,
     runAt: 'document_end'
-  }).then(([currentPageYOffset]) => {
-    let top;
-    if (currentPageYOffset === 0) {
-      if (!(tab.id in savedPageYOffsets)) return;
-      top = savedPageYOffsets[tab.id];
-    } else {
-      savedPageYOffsets[tab.id] = currentPageYOffset;
-      top = 0;
-    }
-    browser.tabs.executeScript({
-      code: `window.scrollTo({top: ${top}})`,
-      runAt: 'document_end'
-    });
   });
-});
-
-browser.tabs.onRemoved.addListener((tabId) => {
-  delete savedPageYOffsets[tabId];
-});
-
-browser.webNavigation.onCommitted.addListener((details) => {
-  if (details.frameId !== 0) return;
-  delete savedPageYOffsets[details.tabId];
 });
