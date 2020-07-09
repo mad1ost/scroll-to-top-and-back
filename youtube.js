536805a4deff83a51d16889bc81bbce923c06d37
mad1ost <mad1ost@mail.ru>
2020-07-09 10:27:12 +0300

    fix for scroll back to Youtube comments

--- youtube.js
+++ youtube.js
@@ -6,12 +6,16 @@ document.addEventListener('mouseup', (event) => {
   const link = event.target;
   const url = document.location;
   
+  if (link.protocol !== url.protocol
+      || link.host !== url.host
+      || link.pathname !== url.pathname) {
+    return;
+  }
+  
   const linkParams = new URLSearchParams(link.search.substring(1));
   const urlParams = new URLSearchParams(url.search.substring(1));
   
-  if (link.protocol === url.protocol
-      && link.host === url.host
-      && link.pathname === url.pathname
+  if (linkParams.has('v')
       && linkParams.get('v') === urlParams.get('v')
       && linkParams.has('t')) {
     window.prevY = window.scrollY;
