24399f8557ed99b539387b71310d372f98b04ba7
mad1ost <mad1ost@mail.ru>
2022-11-29 20:40:29 +0300

    add YouTube plugin support

--- background.js
+++ background.js
@@ -1,24 +1,33 @@
 'use strict';
 
-const code = `
-	!(function() {
-		let prevY = window.prevY;
-		let top;
+let positions = {};
+
+function handleMessage(position, sender) {
+	//if (sender.id === 'scroll-to-top-and-back-youtube@mad1ost')
+	if (sender.id === '{870594cc-ec11-472c-8c1b-da7a75afb063}') {
+		positions[sender.tab.id] = parseInt(position);
+	}
+}
+
+function onClicked(tab) {
+	let code = `!(function() {
+		let savedY = ${(tab.id in positions) ? positions[tab.id] : 'window.savedY'};
+		let newY;
 		if (window.scrollY === 0) {
-			if (!prevY) return;
-			top = prevY;
+			if (!savedY) return;
+			newY = savedY;
 		} else {
-			prevY = window.scrollY;
-			top = 0;
+			savedY = window.scrollY;
+			newY = 0;
 		}
 		window.scrollTo({
-			top: top
+			top: newY
 		});
-		window.prevY = prevY;
+		window.savedY = savedY;
 	}());
-`;
+	`;
+	delete positions[tab.id];
 
-function onClicked() {
 	browser.tabs.executeScript({
 		code: code,
 		runAt: 'document_end'
@@ -26,5 +35,5 @@ function onClicked() {
 }
 
 browser.pageAction.onClicked.addListener(onClicked);
-
 browser.browserAction.onClicked.addListener(onClicked);
+browser.runtime.onMessageExternal.addListener(handleMessage);
