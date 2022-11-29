194fc34e25221856dc7927db0315072d1c19c35b
mad1ost <mad1ost@mail.ru>
2022-11-29 06:45:47 +0300

    use tabs instead of spaces

--- background.js
+++ background.js
@@ -1,28 +1,28 @@
 'use strict';
 
 const code = `
-  !(function() {
-    let prevY = window.prevY;
-    let top;
-    if (window.scrollY === 0) {
-      if (!prevY) return;
-      top = prevY;
-    } else {
-      prevY = window.scrollY;
-      top = 0;
-    }
-    window.scrollTo({
-      top: top
-    });
-    window.prevY = prevY;
-  }());
+	!(function() {
+		let prevY = window.prevY;
+		let top;
+		if (window.scrollY === 0) {
+			if (!prevY) return;
+			top = prevY;
+		} else {
+			prevY = window.scrollY;
+			top = 0;
+		}
+		window.scrollTo({
+			top: top
+		});
+		window.prevY = prevY;
+	}());
 `;
 
 function onClicked() {
-  browser.tabs.executeScript({
-    code: code,
-    runAt: 'document_end'
-  });
+	browser.tabs.executeScript({
+		code: code,
+		runAt: 'document_end'
+	});
 }
 
 browser.pageAction.onClicked.addListener(onClicked);
